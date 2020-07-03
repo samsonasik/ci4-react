2a2f232638a12297ec326efb644fde35d3514c48
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-03 08:46:53 +0700

    handle 404 page

---
+++ app/Filters/NotFoundPage.php
@@ -0,0 +1,21 @@
+<?php namespace App\Filters;
+
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class NotFoundPage implements FilterInterface
+{
+    public function before(RequestInterface $request)
+    {
+        if (ENVIRONMENT !== 'development' && ! class_exists(service('router')->controllerName()))
+        {
+            throw PageNotFoundException::forPageNotFound('Page Not Found');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response)
+    {
+    }
+}
