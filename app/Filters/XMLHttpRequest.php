4f8b4b6f1624dc7c5eb8129985808fc9fd96aefa
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-16 11:29:40 +0700

    update to use codeigniter v4.0.4

--- app/Filters/XMLHttpRequest.php
+++ app/Filters/XMLHttpRequest.php
@@ -6,7 +6,7 @@ use CodeIgniter\Filters\FilterInterface;
 
 class XMLHttpRequest implements FilterInterface
 {
-	public function before(RequestInterface $request)
+	public function before(RequestInterface $request, $arguments = null)
 	{
 		if ($request->isAJAX())
 		{
@@ -16,7 +16,7 @@ class XMLHttpRequest implements FilterInterface
 		return service('response')->setBody(view('layout'));
 	}
 
-	public function after(RequestInterface $request, ResponseInterface $response)
+	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
 	{
 	}
 }
