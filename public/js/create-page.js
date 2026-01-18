4e9159ac8d22e9b0cc2e52ec6b7f039ba79bf688
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-01-18 23:13:01 +0700

    Upgrade to use react-router and react-router-dom v6

--- public/js/create-page.js
+++ public/js/create-page.js
@@ -1,60 +1,86 @@
 const {
-    Component,
-    createElement
+  Component,
+  createElement
 } = React;
+
+const {
+  useLocation,
+  useParams,
+  useNavigate
+} = ReactRouterDOM;
 
 const pagesData = [];
 
-let createPage = (name, title, data = {}) => class Page extends Component {
+// Wrapper to provide hooks to class components
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return createElement(
+      Component,
+      Object.assign({}, props, { router: { location, navigate, params } })
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+const createPage = (name, title, data = {}) => { class Page extends Component {
     constructor(props) {
-        super(props);
-        this.state = Object.assign({ content: ''}, data);
+      super(props);
+      this.state = Object.assign({content: ''}, data);
     }
 
     componentDidMount() {
-        if (name in pagesData) {
-            this.setState({ content: pagesData[name].content });
-            document.title = pagesData[name].title;
+      if (name in pagesData) {
+        this.setState({ content: pagesData[name].content });
+        document.title = pagesData[name].title;
 
-            return;
-        }
+        return;
+      }
 
-        new Promise( (resolve) => {
-            fetch(
-                this.props.location.pathname,
-                {
-                    method: 'GET',
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest',
-                    }
-                }
-            ).then(response =>  resolve(response.text()));
-        }).then(result => {
-            let content = eval("`"+DOMPurify.sanitize(result)+"`");
-            this.setState({ content: content });
-            pagesData[name] = {
-                title: title,
-                content: content
-            };
-            document.title = title;
-        });
+      const pathname = this.props.router.location.pathname;
+
+      new Promise((resolve) => {
+        fetch(
+          pathname,
+          {
+            method: 'GET',
+            headers: {
+              'X-Requested-With': 'XMLHttpRequest',
+            }
+          }
+        ).then(response => resolve(response.text()));
+      }).then(result => {
+        let content = eval("`"+DOMPurify.sanitize(result)+"`");
+        this.setState({ content: content });
+        pagesData[name] = {
+            title: title,
+            content: content
+        };
+        document.title = title;
+      });
     }
 
     render() {
-        return createElement(
-            'div',
-            {
-                className : "app-content",
-            },
-            createElement(
-                "main",
-                {
-                    className : "container"
-                },
-                HTMLReactParser(this.state.content)
-            )
-        );
+      return createElement(
+        'div',
+        {
+          className: 'app-content',
+        },
+        createElement(
+          'main',
+          {
+            className: 'container'
+          },
+          HTMLReactParser(this.state.content)
+        )
+      );
     }
-}
+  }
+
+  return withRouter(Page);
+};
 
 export default createPage;
