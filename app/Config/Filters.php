ad3435d5120638d418bbe438732e86b4b489ff47
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-01-18 23:47:06 +0700

    patch

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -74,6 +74,7 @@ class Filters extends BaseFilters
      */
     public array $globals = [
         'before' => [
+            'toolbar',
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
